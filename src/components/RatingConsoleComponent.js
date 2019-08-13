450ea83d73a5a4bc1da711a046e1a7d4197a3c8f
Michael Ian Fernandes <mfern93@uw.edu>
2019-08-13 01:24:36 -0700

    this is how to do it as a function component, a bit slippery

--- src/components/RatingConsoleComponent.js
+++ src/components/RatingConsoleComponent.js
@@ -1,57 +1,25 @@
 import React, { Component } from 'react';
 
-class RatingConsole extends Component {
 
-  constructor(props) {
-    super(props);
+export default function RatingConsole(props) {
+  const button = props.codes.map(({ id }) => {
+    return (
+      <button
+        key={id}
+        value={id}
+        className="ratingBtn"
+        onClick={() =>
+          props.changeCode(id)
+        }>
+        {id}
+      </button>
+    );
+  });
 
-    this.state = {
-      currentCode: "Pooooop"
-    };
-    this.onChangeCode = this.onChangeCode.bind(this);
-  }
+  return (
+    <div>
+      {button}
+    </div>
+  );
 
-  onChangeCode(event) {
-    this.props.changeCode(event.target.value);
-    console.log(event.target.value);
-  }
-
-  // handleClick(event) {
-  //   this.setState (
-  //     {currentCode: event.target.value}
-  //   );
-  // }
-
-  render() {
-
-    if (this.props.codes != null) {
-
-      const button = this.props.codes.map(({ id }) => {
-        return (
-          <button
-            key={id}
-            value={id}
-            className="ratingBtn"
-            onClick={
-              this.onChangeCode
-            }>
-            {id}
-          </button>
-        );
-      });
-
-      return (
-        <div>
-          {button}
-        </div>
-      );
-
-    } else {
-      return (
-        <div></div>
-      );
-    }
-  }
 }
-
-export default RatingConsole;
