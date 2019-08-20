5f0f1b5ba6f4f742a841aecd08b800088da3bb7d
vivianlu <vlu2009@gmail.com>
2019-08-19 22:41:23 -0700

    adjust buttons

--- src/components/RatingDialsComponent.js
+++ src/components/RatingDialsComponent.js
@@ -5,16 +5,16 @@ class RatingDials extends Component {
   render() {
     return (
       <div>
-        <button onClick={this.props.ratingChange.bind(null, 1)}>1</button>
-        <button onClick={this.props.ratingChange.bind(null, 2)}>2</button>
-        <button onClick={this.props.ratingChange.bind(null, 3)}>3</button>
-        <button onClick={this.props.ratingChange.bind(null, 4)}>4</button>
-        <button onClick={this.props.ratingChange.bind(null, 5)}>5</button>
-        <button onClick={this.props.ratingChange.bind(null, 6)}>6</button>
-        <button onClick={this.props.ratingChange.bind(null, 7)}>7</button>
-        <button onClick={this.props.ratingChange.bind(null, 8)}>8</button>
-        <button onClick={this.props.ratingChange.bind(null, 9)}>9</button>
-        <button onClick={this.props.ratingChange.bind(null, 10)}>10</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 1)}>1</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 2)}>2</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 3)}>3</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 4)}>4</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 5)}>5</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 6)}>6</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 7)}>7</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 8)}>8</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 9)}>9</button>
+        <button className="ratingBtn" onClick={this.props.ratingChange.bind(null, 10)}>10</button>
       </div>
     );
   }
