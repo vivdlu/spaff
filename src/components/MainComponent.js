d9c5142cacc0b286ec8f735d6d5af36c0bd7db63
vivianlu <vlu2009@gmail.com>
2019-09-27 16:12:44 -0700

    add svg icon for device rotation message

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import RatingConsole from "./RatingConsoleComponent";
 import Navbar from "./NavbarComponent";
-import { Container, Typography } from "@material-ui/core";
+import { Container, Typography, SvgIcon } from "@material-ui/core";
 import DeviceOrientation, { Orientation } from "react-screen-orientation";
-import RestorePageIcon from "@material-ui/icons/RestorePage";
 
 class Main extends Component {
   render() {
@@ -17,7 +16,14 @@ class Main extends Component {
             </Orientation>
             <Orientation orientation="portrait" alwaysRender={false}>
               <Container className="portrait-container">
-                <RestorePageIcon fontSize="large" />
+                <SvgIcon
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="48"
+                  height="48"
+                  viewBox="0 0 48 48"
+                >
+                  <path d="M32.97 5.03C39.5 8.13 44.19 14.48 44.9 22h3C46.88 9.68 36.58 0 24 0c-.45 0-.88.04-1.33.07L30.3 7.7l2.67-2.67zM20.46 3.49c-1.17-1.17-3.07-1.17-4.24 0L3.49 16.22c-1.17 1.17-1.17 3.07 0 4.24L27.53 44.5c1.17 1.17 3.07 1.17 4.24 0L44.5 31.77c1.17-1.17 1.17-3.07 0-4.24L20.46 3.49zm9.2 38.89L5.61 18.34 18.34 5.61l24.04 24.04-12.72 12.73zm-14.63.59C8.5 39.87 3.81 33.52 3.1 26h-3C1.12 38.32 11.42 48 24 48c.45 0 .88-.04 1.33-.07L17.7 40.3l-2.67 2.67z" />
+                </SvgIcon>
                 <Typography className="portrait-text" variant="h4">
                   Please rotate your device.
                 </Typography>
