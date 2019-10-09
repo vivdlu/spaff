790f085eb6b4f543a72fed95e45b432b26862e3d
vivianlu <vlu2009@gmail.com>
2019-10-08 23:44:11 -0700

    Remove orientation component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,20 +2,12 @@ import React, { Component } from "react";
 import RatingConsole from "./RatingConsoleComponent";
 import RotateMessage from "./RotateMessageComponent";
 import { Container } from "@material-ui/core";
-import DeviceOrientation, { Orientation } from "react-screen-orientation";
 
 class Main extends Component {
   render() {
     return (
       <Container maxWidth="lg">
-        <DeviceOrientation lockOrientation={"landscape"}>
-          <Orientation orientation="portrait" alwaysRender={false}>
-            <RotateMessage />
-          </Orientation>
-          <Orientation orientation="landscape" alwaysRender={true}>
-            <RatingConsole />
-          </Orientation>
-        </DeviceOrientation>
+        <RatingConsole />
       </Container>
     );
   }
