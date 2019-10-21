727fb7e12a8835c9cb9c4fc0741c4b6168ac92ed
vivianlu <vlu2009@gmail.com>
2019-10-20 18:03:38 -0700

    Updates

---
+++ precache-manifest.bc009b518a5d4a4b0683d9639d554c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a1d7b2d85d3125033e6afb28ffa9d7",
+    "url": "/spaff/index.html"
+  },
+  {
+    "revision": "b308dca3d843781fbcc1",
+    "url": "/spaff/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fdeae904688ef06d7818",
+    "url": "/spaff/static/css/main.a12c0b2c.chunk.css"
+  },
+  {
+    "revision": "b308dca3d843781fbcc1",
+    "url": "/spaff/static/js/2.3434caa1.chunk.js"
+  },
+  {
+    "revision": "fdeae904688ef06d7818",
+    "url": "/spaff/static/js/main.c6151777.chunk.js"
+  },
+  {
+    "revision": "e5ee12aaa05ec290b295",
+    "url": "/spaff/static/js/runtime~main.6f00aec4.js"
+  }
+]);
