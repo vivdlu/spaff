c4543d2907861d95ffe20dd56861f620cbf813e8
vivianlu <vlu2009@gmail.com>
2019-10-20 18:07:11 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spaff/precache-manifest.bc009b518a5d4a4b0683d9639d554c72.js"
+  "/spaff/precache-manifest.2f9e05bdeb35b6629ef42e0a00a1f4b9.js"
 );
 
 self.addEventListener('message', (event) => {
