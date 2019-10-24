364371dd4d4965c8671b2edf5849b3c2f1c06387
vivianlu <vlu2009@gmail.com>
2019-10-24 00:07:05 -0700

    Updates

--- precache-manifest.652663d47e4c127dfbdc9b48e5f0a6a4.js
+++ precache-manifest.652663d47e4c127dfbdc9b48e5f0a6a4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b97c4d8bf6c3f8420279aa8795a31e7b",
+    "revision": "841e1ed6aa203cbf17a92e3ca06f2999",
     "url": "/spaff/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spaff/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "91f36333d9e8963e9e67",
+    "revision": "9b9a59ade73709563130",
     "url": "/spaff/static/css/main.7f4cd9cd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spaff/static/js/2.3b8550c0.chunk.js"
   },
   {
-    "revision": "91f36333d9e8963e9e67",
-    "url": "/spaff/static/js/main.0e2ab135.chunk.js"
+    "revision": "9b9a59ade73709563130",
+    "url": "/spaff/static/js/main.f5d05ea6.chunk.js"
   },
   {
     "revision": "e5ee12aaa05ec290b295",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spaff/precache-manifest.e53f1d7a056fbac7b7dd8bde623a720f.js"
+  "/spaff/precache-manifest.652663d47e4c127dfbdc9b48e5f0a6a4.js"
 );
 
 self.addEventListener('message', (event) => {
