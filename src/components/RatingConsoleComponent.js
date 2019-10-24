08e01b139038191ce329f1c01e81d06071e826e9
vivianlu <vlu2009@gmail.com>
2019-10-23 20:11:32 -0700

    change from onStart to onReady in ReactPlayer component method

--- src/components/RatingConsoleComponent.js
+++ src/components/RatingConsoleComponent.js
@@ -92,7 +92,7 @@ class RatingConsole extends Component {
             url={this.state.src}
             width="100%"
             height="100%"
-            onStart={this.handleVideoStart.bind(this)}
+            onReady={this.handleVideoStart.bind(this)}
             onProgress={this.handleTimeChange.bind(this)}
             onEnded={this.handleCodingEnd.bind(this)}
             playing={this.state.playing}
